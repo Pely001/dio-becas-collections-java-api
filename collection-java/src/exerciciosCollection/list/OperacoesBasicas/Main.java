33c819ac05848131a1b4e4a45010502deb7d6d6d
Pely001 <pelypedrohenrique1@gmail.com>
2023-09-21 21:41:04 -0300

    Criação do Método Main e teste dos métodos

---
+++ collection-java/src/exerciciosCollection/list/OperacoesBasicas/Main.java
@@ -0,0 +1,21 @@
+package exerciciosCollection.list.OperacoesBasicas;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        listaTarefa.adicionarTarefa("Tarefa 4");
+        listaTarefa.adicionarTarefa("Tarefa 5");
+
+        System.out.println("Número total de elementos na lista é: " + listaTarefa.oberNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("Número total de elementos na lista é: " + listaTarefa.oberNumeroTotalTarefas());
+
+        listaTarefa.oberDescricoesTarefas();
+        
+    }
+}
