c2be828c0a8ec79b3210d8a4b16b0f0513d03948
Pely001 <pelypedrohenrique1@gmail.com>
2023-09-21 22:54:56 -0300

    Exercício Finalizado

---
+++ collection-java/src/exerciciosCollection/list/OperacoesBasicas/carrinhoCompras/Main.java
@@ -0,0 +1,25 @@
+package exerciciosCollection.list.OperacoesBasicas.carrinhoCompras;
+
+public class Main {
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        // Adicionar
+
+        carrinhoDeCompras.adicionarItem("Mouse", 9.99, 1);
+        carrinhoDeCompras.adicionarItem("Cabo USB", 2.0, 10);
+        carrinhoDeCompras.adicionarItem("Teclado", 29.99, 1);
+        carrinhoDeCompras.adicionarItem("Imã de Geladeira", 1.0, 1);
+
+        // Exibir
+
+        carrinhoDeCompras.exibirItens();
+
+        // Remover
+        carrinhoDeCompras.removerItem("Imã de Geladeira");
+
+        // Valor Total
+        System.out.println("O valor total da compra é: " + carrinhoDeCompras.calcularValorTotal());
+        
+    }
+}
